a521e890ef53c8560d723710eb2f17e32104bc50
anurag <semwalanurag26@gmail.com>
2024-06-29 17:55:24 +0530

    Add updateNotes function
    The updateNotes function adds the funtionality to add notes to a state list "notes" and display them as soon as they are added to the "notes" list.

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,34 @@ function App() {
 
   function updateContent(e) {
     let { value } = e.target;
-    setTitle(value);
+    setContent(value);
   }
-  // function updateNotes(){
-  //   setNotes((prev)=>{
-  //     return [...prev]
-  //   })
-  // }
+  function updateNotes(e) {
+    setNotes((prev) => {
+      return [...prev, [title, content]];
+    });
+
+    e.preventDefault();
+  }
 
   return (
     <div id="App">
       <div id="app-wrapper">
         <h1 className="app-title">Keeper</h1>
-        <form action="submit">
+        <form action="submit" onSubmit={updateNotes}>
           <input name="title" onChange={updateTitle} value={title} type="text" placeholder="Title" />
           <input name="content" onChange={updateContent} value={content} type="text" placeholder="Your note" />
           <button type="submit">Add</button>
         </form>
         <div className="card-container">
-          <div className="card">
-            <h1 className="title">This is title</h1>
-            <p className="content">This the content of the note.</p>
-          </div>
+          {notes.map((note, index) => {
+            return (
+              <div className="card">
+                <h1 className="title">{note[0]}</h1>
+                <p className="content">{note[1]}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
